4acf0d0025e0221940a57eea4ed1d1963e4e34c3
MaerS <91330403+KamilKonopski@users.noreply.github.com>
2022-12-14 22:25:52 +0100

    add routes and server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 

4e60a07fa430b550c7ccc9ae65d4a2de86f17cdb
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-07 17:59:24 +0100

    add charakters route type get

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const filmsActions = require("../actions/films");
+const charaktersActions = require("../actions/charakters");
 
 const router = express.Router();
 
 // fetch films
 router.get("/films", filmsActions.getFilms);
+
+//fetch charakters
+router.get("/charakters", charaktersActions.getCharakters);
 
 module.exports = router;

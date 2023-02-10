898ec047974b00df691a136f36e4fc359ae7c922
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-10 18:09:03 +0100

    add function to find single charakter

--- actions/charakters.js
+++ actions/charakters.js
@@ -1,8 +1,16 @@
 const Charakter = require("../db/models/charakter");
 
 module.exports = {
-	getCharakters(req, res) {
+	getAllCharakters(req, res) {
 		Charakter.find({}, (err, doc) => {
+			res.status(200).json(doc);
+		});
+	},
+
+	getSingleCharakter(req, res) {
+		const id = req.params.id;
+
+		Charakter.findOne({ _id: id }, (err, doc) => {
 			res.status(200).json(doc);
 		});
 	},

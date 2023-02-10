187cfc0fe305e952391dabc7e10c38f35b15479e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-10 18:05:52 +0100

    add new function to find single film

--- actions/films.js
+++ actions/films.js
@@ -1,8 +1,16 @@
 const Film = require("../db/models/film");
 
 module.exports = {
-	getFilms(req, res) {
+	getAllFilms(req, res) {
 		Film.find({}, (err, doc) => {
+			res.status(200).json(doc);
+		});
+	},
+
+	getSingleFilm(req, res) {
+		const id = req.params.id;
+
+		Film.findOne({ _id: id }, (err, doc) => {
 			res.status(200).json(doc);
 		});
 	},

7200f3295521865035aa781e1863359fc0bedd98
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-10 18:05:20 +0100

    add single film route

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,16 @@ const charaktersActions = require("../actions/charakters");
 
 const router = express.Router();
 
+// general route
+router.get("/", (req, res) => {
+	res.send("Server is running...");
+});
+
 // fetch films
-router.get("/films", filmsActions.getFilms);
+router.get("/films", filmsActions.getAllFilms);
+
+//fetch single film
+router.get("/films/:id", filmsActions.getSingleFilm);
 
 //fetch charakters
 router.get("/charakters", charaktersActions.getCharakters);

16220192af1e49ca6d01a1e85ff14bebd1a82007
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-30 13:55:48 +0200

    add category and product routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import RootLayout from "./components/Layout/RootLayout";
 import HomePage from "./pages/HomePage";
+import ProductCategoryPage from "./pages/ProductCategoryPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 import RegisterPage from "./pages/RegisterPage";
 import SignupPage from "./pages/SignupPage";
 
@@ -23,6 +20,8 @@ const App: React.FC = () => {
 						<Route index={true} element={<HomePage />} />
 						<Route path="sign-up" element={<SignupPage />} />
 						<Route path="register" element={<RegisterPage />} />
+						<Route path="category/:id" element={<ProductCategoryPage />} />
+						<Route path="product/:id" element={<ProductDetailsPage />} />
 					</Route>
 				</Routes>
 			</Router>

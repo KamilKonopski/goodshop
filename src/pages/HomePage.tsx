d0291a291867e7e4e14b547d004305f397d895b6
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-30 15:41:41 +0200

    add Home page component, fetch data

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import ContentFilters from "../components/ContentFilters/ContentFilters";
+import Products from "../components/Products/Products";
+
+import { IProducts } from "src/interfaces/products";
+
+const HomePage: React.FC = () => {
+	const [products, setProducts] = useState<IProducts[] | []>([]);
+	const [hasMore, setHasMore] = useState<boolean>(true);
+	const [page, setPage] = useState<number>(0);
+	const elementRef = useRef<HTMLDivElement>(null);
+
+	const onIntersection = (entries: IntersectionObserverEntry[]) => {
+		const firstEntry = entries[0];
+		if (firstEntry.isIntersecting && hasMore) {
+			fetchMoreProducts();
+		}
+	};
+
+	const fetchMoreProducts = async () => {
+		const response = await fetch(
+			`https://dummyjson.com/products?limit=16&skip=${page * 16}`
+		);
+		const { products }: { products: IProducts[] } = await response.json();
+
+		if (products.length === 0) {
+			setHasMore(false);
+		} else {
+			setProducts((prev) => [...prev, ...products]);
+			setPage((prev) => prev + 1);
+		}
+	};
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(onIntersection);
+		if (observer && elementRef.current) {
+			observer.observe(elementRef.current);
+		}
+
+		return () => {
+			if (observer) {
+				observer.disconnect();
+			}
+		};
+	}, [products]);
+
+	return (
+		<main className="main">
+			<ContentFilters />
+			<Products products={products} hasMore={hasMore} ref={elementRef} />
+		</main>
+	);
+};
+
+export default HomePage;

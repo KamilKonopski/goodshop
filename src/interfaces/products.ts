c090af59fefbead9a66c361ceaa4b71a55294554
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-30 12:12:38 +0200

    add products interface

---
+++ src/interfaces/products.ts
@@ -0,0 +1,13 @@
+export interface IProducts {
+	id: number;
+	title: string;
+	brand: string;
+	price: number;
+	category: string;
+	description: string;
+	discountPercentage: number;
+	rating: number;
+	stock: number;
+	thumbnail: string;
+	images: string[];
+}

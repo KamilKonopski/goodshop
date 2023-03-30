396250e6f170d030299b32062a0eb79bf610f07d
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-30 15:42:28 +0200

    remove logic

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,56 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef } from "react";
+import { Link } from "react-router-dom";
 
 import { IProducts } from "src/interfaces/products";
 
 import classes from "./Products.module.scss";
 
-const Products: React.FC = () => {
-	const [products, setProducts] = useState<IProducts[] | []>([]);
-	const [hasMore, setHasMore] = useState<boolean>(true);
-	const [page, setPage] = useState<number>(0);
-	const elementRef = useRef<HTMLDivElement>(null);
+interface IProps {
+	products: IProducts[];
+	hasMore?: boolean;
+	ref: HTMLDivElement;
+}
 
-	const onIntersection = (entries: IntersectionObserverEntry[]) => {
-		const firstEntry = entries[0];
-		if (firstEntry.isIntersecting && hasMore) {
-			fetchMoreProducts();
-		}
-	};
-
-	const fetchMoreProducts = async () => {
-		const response = await fetch(
-			`https://dummyjson.com/products?limit=16&skip=${page * 16}`
-		);
-		const { products }: { products: IProducts[] } = await response.json();
-
-		if (products.length === 0) {
-			setHasMore(false);
-		} else {
-			setProducts((prev) => [...prev, ...products]);
-			setPage((prev) => prev + 1);
-		}
-	};
-
-	useEffect(() => {
-		const observer = new IntersectionObserver(onIntersection);
-		if (observer && elementRef.current) {
-			observer.observe(elementRef.current);
-		}
-
-		return () => {
-			if (observer) {
-				observer.disconnect();
-			}
-		};
-	}, [products]);
-
-	return (
-		<section className={classes.products}>
-			<h2 className={classes["products__found"]}>
-				{products.length} product found
-			</h2>
-			{products.map((product) => (
-				<div key={product.id} className={classes.product}>
+const Products = forwardRef<HTMLDivElement, IProps>((props, ref) => (
+	<section className={classes.products}>
+		<h2 className={classes["products__found"]}>
+			{props.products.length} product found
+		</h2>
+		{props.products.map((product) => (
+			<Link key={product.id} to={`/product/${product.id}`}>
+				<div className={classes.product}>
 					<div className={classes["product__image-container"]}>
 						<img
 							className={classes["product__image"]}
@@ -65,10 +33,12 @@ const Products: React.FC = () => {
 					</div>
 					<span className={classes["product__price"]}>${product.price}</span>
 				</div>
-			))}
-			{hasMore && <div ref={elementRef}>Loading more products...</div>}
-		</section>
-	);
-};
+			</Link>
+		))}
+		{props.hasMore && <div ref={ref}>Loading more products...</div>}
+	</section>
+));
+
+Products.displayName = "Products";
 
 export default Products;

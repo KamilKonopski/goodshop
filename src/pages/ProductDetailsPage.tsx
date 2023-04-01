638a18706741857b85b4a1f7a705a3c563c40554
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-01 21:04:05 +0200

    add component

---
+++ src/pages/ProductDetailsPage.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ProductDetailts from "../components/Products/ProductDetailts";
+
+import { IProducts } from "src/interfaces/products";
+
+import useDocumentTitle from "../hooks/useDocumentTitle";
+
+const ProductDetailsPage: React.FC = () => {
+	const [product, setProduct] = useState<IProducts | null>(null);
+	const { id } = useParams();
+
+	useDocumentTitle(`- ${product && product.title}`);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(`https://dummyjson.com/products/${id}`);
+			try {
+				const data: IProducts = await response.json();
+
+				setProduct(data);
+			} catch (err) {
+				//
+			}
+		};
+
+		fetchData();
+	}, [id]);
+
+	return <>{product && <ProductDetailts product={product} />}</>;
+};
+
+export default ProductDetailsPage;

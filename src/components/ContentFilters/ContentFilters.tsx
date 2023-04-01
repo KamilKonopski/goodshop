57202e83ec8eb106ab3b679381a03640697acb5f
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-02 00:05:25 +0200

    add component

---
+++ src/components/ContentFilters/ContentFilters.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./ContentFilters.module.scss";
+
+const ContentFilters: React.FC = () => {
+	return (
+		<aside className={classes.filters}>
+			ContentFilters Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Laudantium aliquid sapiente, illum inventore sunt beatae magnam soluta
+			facere similique. Enim quaerat, dicta eveniet officia ab culpa dolores
+			debitis architecto consequatur.
+		</aside>
+	);
+};
+
+export default ContentFilters;

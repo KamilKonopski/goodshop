b049d2dea578dcc362a474f6f516b5bc6643ed96
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-04 00:36:34 +0200

    fix import

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ProductDetailts from "../components/Products/ProductDetailts";
+import ProductDetailts from "../components/Products/ProductDetails";
 
 import { IProducts } from "src/interfaces/products";
 

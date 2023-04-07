b48644b6aefae15d9ab7241e52fa22e47d88d325
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-07 20:32:48 +0200

    add DashboardPage component

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Settings from "../components/Settings/Settings";
+
+const DashboardPage: React.FC = () => {
+	return <Settings />;
+};
+
+export default DashboardPage;

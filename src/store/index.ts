71c9033c850839eccbad5a6f28fec6222e4ef8d3
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-09 01:56:45 +0200

    add cart reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,10 +3,15 @@ import { useDispatch } from "react-redux";
 
 import authSlice from "./auth-slice";
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 export const createStore = () =>
 	configureStore({
-		reducer: { auth: authSlice.reducer, ui: uiSlice.reducer },
+		reducer: {
+			auth: authSlice.reducer,
+			ui: uiSlice.reducer,
+			cart: cartSlice.reducer,
+		},
 	});
 
 const store = createStore();

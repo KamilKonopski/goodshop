36fa8c80f0ae842af6682b2e91a50bf2f593681b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-13 00:07:20 +0200

    add simply jsx and favouriteProducts component

--- src/components/ContentFilters/ContentFilters.tsx
+++ src/components/ContentFilters/ContentFilters.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+import FavouriteProducts from "../FavouriteProducts/FavouriteProducts";
 
 import classes from "./ContentFilters.module.scss";
 
 const ContentFilters: React.FC = () => {
+	const [showFavourite, setShowFavourite] = useState<boolean>(false);
+
 	return (
 		<aside className={classes.filters}>
-			ContentFilters Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Laudantium aliquid sapiente, illum inventore sunt beatae magnam soluta
-			facere similique. Enim quaerat, dicta eveniet officia ab culpa dolores
-			debitis architecto consequatur.
+			<h2>Filter</h2>
+			<div>
+				<button onClick={() => setShowFavourite((prev) => !prev)}>
+					<span>Favourite Searches</span>
+					<RiArrowDropDownLine
+						style={
+							showFavourite
+								? { transform: "rotate(0)", transition: "all 0.2s linear" }
+								: { transform: "rotate(-90deg)", transition: "all 0.2s linear" }
+						}
+						size={20}
+						color="#a5a8ad"
+					/>
+				</button>
+				{showFavourite && <FavouriteProducts />}
+			</div>
 		</aside>
 	);
 };

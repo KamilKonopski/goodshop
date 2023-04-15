63d463600ba4ef83f698ab68d3128cda893c3865
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-15 14:15:49 +0200

    add hook to change title

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 import Settings from "../components/Settings/Settings";
 
+import useDocumentTitle from "../hooks/useDocumentTitle";
+
 const DashboardPage: React.FC = () => {
+	useDocumentTitle("- dashboard");
+
 	return <Settings />;
 };
 

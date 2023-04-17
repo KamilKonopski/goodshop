0669a92f190e963d969e3466ca1fd8d4feda2939
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-04-17 20:01:11 +0200

    rename

---
+++ src/components/ProductFilters/FilterFavourite.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../../store";
+
+import classes from "./FilterFavourite.module.scss";
+
+const FilterFavourite: React.FC = () => {
+	const favProducts = useSelector(
+		(state: RootState) => state.ui.favouriteProducts
+	);
+
+	return (
+		<ul className={classes.favourite}>
+			{favProducts.map((fav) => (
+				<li className={classes["favourite__item"]} key={fav.id}>
+					<Link
+						className={classes["favourite__link"]}
+						to={`/product/${fav.id}`}
+					>
+						{fav.title}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default FilterFavourite;
